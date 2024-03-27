154fc10e12e88ba57c49cd56f261abe6ed5f34ef
XIA-LIYI <xialiyi@nus.edu.sg>
2024-03-27 16:42:05 +0800

    Modify start

--- delete.sh
+++ delete.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
-# mapfile -t workers < machines.txt
-
-# for worker in "${workers[@]}"; do
-#     script_file="${worker}_script.sh"
-#     rm "./workers/$script_file"
-# done
-
 rm ./workers/*.sh
 rm ./workers/*.out
 rm ./workers/*.log
 rm *.out
 rm ./controller/*.sh
 rm ./controller/*.out
-git restore .
