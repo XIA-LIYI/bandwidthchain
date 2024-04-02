554f94459567e232fe6c284ab9ce509aeb755d90
XIA-LIYI <xialiyi@nus.edu.sg>
2024-04-02 18:58:23 +0800

    Add log

--- start.py
+++ start.py
@@ -14,6 +14,7 @@ def get_nodes(partition):
     long_nodes = get_nodes_with_partition("long")
     selected_nodes = []
     results = []
+    print(long_nodes)
     for node in long_nodes:
         selected_nodes.append(node)
         results.append([node, "long"])
